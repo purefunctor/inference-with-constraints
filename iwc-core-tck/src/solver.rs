6811cb737e3d4e23ef76ebc2aceed9657e23bd1e
Justin Garcia <purefunctor@gmail.com>
2023-07-01 23:25:24 +0800

    Extract solving to a step function

--- iwc-core-tck/src/solver.rs
+++ iwc-core-tck/src/solver.rs
@@ -1,16 +1,24 @@
 pub mod solve;
 
+use std::collections::HashMap;
+
 use iwc_core_ast::ty::{Assertion, TypeIdx};
 
 use crate::context::Context;
 
 pub struct Solver {
     pub(crate) context: Context,
+    pub(crate) unifications: HashMap<usize, TypeIdx>,
+    pub(crate) unsolved_deep: Vec<(usize, usize)>,
 }
 
 impl Solver {
     pub fn new(context: Context) -> Self {
-        Self { context }
+        Self {
+            context,
+            unifications: HashMap::new(),
+            unsolved_deep: Vec::new(),
+        }
     }
 }
 
--- iwc-core-tck/src/solver/solve.rs
+++ iwc-core-tck/src/solver/solve.rs
@@ -1,71 +1,63 @@
-use std::collections::HashMap;
-
-use iwc_core_ast::ty::{TypeIdx, pretty::pretty_print_ty};
-
 use super::{Constraint, Solver};
 
-type Unifications = HashMap<usize, TypeIdx>;
-
-type Unsolved = Vec<(usize, usize)>;
-
 impl Solver {
-    fn take_constraints(&mut self) -> Vec<Constraint> {
+    pub(crate) fn take_constraints(&mut self) -> Vec<Constraint> {
         std::mem::take(&mut self.context.volatile.constraints)
+    }
+
+    pub(crate) fn step(&mut self, constraints: Vec<Constraint>) -> anyhow::Result<Vec<Constraint>> {
+        for constraint in constraints {
+            match constraint {
+                Constraint::ClassAssertion(_) => continue,
+                Constraint::UnifyDeep(u_name, t_name) => {
+                    let u_ty = self.unifications.get(&u_name);
+                    let t_ty = self.unifications.get(&t_name);
+                    match (u_ty, t_ty) {
+                        (Some(u_ty), Some(t_ty)) => {
+                            self.context.unify(*u_ty, *t_ty)?;
+                        }
+                        (None, Some(t_ty)) => {
+                            self.unifications.insert(u_name, *t_ty);
+                        }
+                        (Some(u_ty), None) => {
+                            self.unifications.insert(t_name, *u_ty);
+                        }
+                        (None, None) => {
+                            self.unsolved_deep.push((u_name, t_name));
+                        }
+                    }
+                }
+                Constraint::UnifySolve(name, ty) => {
+                    self.unifications.insert(name, ty);
+                }
+            }
+        }
+
+        let mut constraints = self.take_constraints();
+
+        self.unsolved_deep.retain(|(u_name, t_name)| {
+            let u_ty = self.unifications.get(u_name);
+            let t_ty = self.unifications.get(t_name);
+            if u_ty.is_some() || t_ty.is_some() {
+                constraints.push(Constraint::UnifyDeep(*u_name, *t_name));
+                false
+            } else {
+                true
+            }
+        });
+
+        Ok(constraints)
     }
 
     pub fn solve(&mut self) -> anyhow::Result<()> {
         let mut constraints = self.take_constraints();
-        let mut unifications = Unifications::new();
-        let mut unsolved = Unsolved::new();
 
         loop {
-            for constraint in constraints {
-                match constraint {
-                    Constraint::ClassAssertion(_) => continue,
-                    Constraint::UnifyDeep(u_name, t_name) => {
-                        let u_ty = unifications.get(&u_name);
-                        let t_ty = unifications.get(&t_name);
-                        match (u_ty, t_ty) {
-                            (Some(u_ty), Some(t_ty)) => {
-                                self.context.unify(*u_ty, *t_ty)?;
-                            }
-                            (None, Some(t_ty)) => {
-                                unifications.insert(u_name, *t_ty);
-                            }
-                            (Some(u_ty), None) => {
-                                unifications.insert(t_name, *u_ty);
-                            }
-                            (None, None) => {
-                                unsolved.push((u_name, t_name));
-                            }
-                        }
-                    }
-                    Constraint::UnifySolve(name, ty) => {
-                        unifications.insert(name, ty);
-                    }
-                }
-            }
-
-            constraints = self.take_constraints();
-
-            unsolved.retain(|(u_name, t_name)| {
-                let u_ty = unifications.get(u_name);
-                let t_ty = unifications.get(t_name);
-                if u_ty.is_some() || t_ty.is_some() {
-                    constraints.push(Constraint::UnifyDeep(*u_name, *t_name));
-                    false
-                } else {
-                    true
-                }
-            });
+            constraints = self.step(constraints)?;
 
             if constraints.is_empty() {
                 break;
             }
-        }
-
-        for (name, ty) in unifications {
-            println!("?{} ~ {}", name, pretty_print_ty(&self.context.volatile.type_arena, ty));
         }
 
         Ok(())
