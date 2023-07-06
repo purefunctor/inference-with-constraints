ea76617f4bd38a961bc40be5ca9f286fd9408847
Justin Garcia <purefunctor@gmail.com>
2023-07-06 16:52:07 +0800

    Implement constraint solving for unifications

--- iwc-core-elaborate/src/lib.rs
+++ iwc-core-elaborate/src/lib.rs
@@ -6,10 +6,39 @@ pub mod unify;
 
 #[cfg(test)]
 mod tests {
-    use crate::context::Context;
+    use iwc_core_ast::ty::Type;
+    use iwc_core_constraint::Constraint;
+
+    use crate::{context::Context, solve::Solve};
 
     #[test]
     fn api_construction() {
-        let _ = Context::default();
+        let ref mut context = Context::default();
+
+        let int = context
+            .volatile
+            .type_arena
+            .allocate(Type::Constructor { name: "Int".into() });
+
+        context
+            .constraints
+            .push(Constraint::UnifyDeep(0, 1))
+            .unwrap();
+        context
+            .constraints
+            .push(Constraint::UnifySolve(1, int))
+            .unwrap();
+
+        let mut solve = Solve::new(context);
+
+        solve.step();
+
+        dbg!(&solve.unification_solved);
+        dbg!(&solve.unification_errors);
+
+        solve.step();
+
+        dbg!(&solve.unification_solved);
+        dbg!(&solve.unification_errors);
     }
 }
--- iwc-core-elaborate/src/solve.rs
+++ iwc-core-elaborate/src/solve.rs
@@ -7,10 +7,10 @@ use iwc_core_error::UnifyError;
 use crate::{context::Context, unify::Unify};
 
 pub struct Solve<'context> {
-    context: &'context mut Context,
-    unification_solved: HashMap<usize, TypeIdx>,
-    unification_deferred: Vec<(usize, usize)>,
-    unification_errors: Vec<UnifyError>,
+    pub(crate) context: &'context mut Context,
+    pub(crate) unification_solved: HashMap<usize, TypeIdx>,
+    pub(crate) unification_deferred: Vec<(usize, usize)>,
+    pub(crate) unification_errors: Vec<UnifyError>,
 }
 
 impl<'context> Solve<'context> {
@@ -27,33 +27,37 @@ impl<'context> Solve<'context> {
         Unify::new(self.context)
     }
 
-    pub(crate) fn step(&mut self, constraint: Constraint) {
-        match constraint {
-            Constraint::ClassEntail(_) => todo!(),
-            Constraint::ClassEvidence(_) => todo!(),
-            Constraint::UnifyDeep(t_name, u_name) => {
-                let t_idx = self.unification_solved.get(&t_name).copied();
-                let u_idx = self.unification_solved.get(&u_name).copied();
-                match (t_idx, u_idx) {
-                    (Some(t_idx), Some(u_idx)) => {
-                        self.as_unify().unify(t_idx, u_idx);
-                    }
-                    (None, Some(u_idx)) => {
-                        self.unification_solved.insert(t_name, u_idx);
-                    }
-                    (Some(t_idx), None) => {
-                        self.unification_solved.insert(u_name, t_idx);
-                    }
-                    (None, None) => {
-                        self.unification_deferred.push((u_name, t_name));
+    pub(crate) fn step(&mut self) {
+        while let Ok(constraint) = self.context.constraints.pop() {
+            match constraint {
+                Constraint::ClassEntail(_) => todo!(),
+                Constraint::ClassEvidence(_) => todo!(),
+                Constraint::UnifyDeep(t_name, u_name) => {
+                    let t_idx = self.unification_solved.get(&t_name).copied();
+                    let u_idx = self.unification_solved.get(&u_name).copied();
+                    match (t_idx, u_idx) {
+                        (Some(t_idx), Some(u_idx)) => {
+                            self.as_unify().unify(t_idx, u_idx);
+                        }
+                        (None, Some(u_idx)) => {
+                            self.unification_solved.insert(t_name, u_idx);
+                        }
+                        (Some(t_idx), None) => {
+                            self.unification_solved.insert(u_name, t_idx);
+                        }
+                        (None, None) => {
+                            // Avoids infinite loops with unification
+                            // variables that never get solved.
+                            self.unification_deferred.push((t_name, u_name));
+                        }
                     }
                 }
-            }
-            Constraint::UnifySolve(name, ty) => {
-                self.unification_solved.insert(name, ty);
-            }
-            Constraint::UnifyError(error) => {
-                self.unification_errors.push(error);
+                Constraint::UnifySolve(name, ty) => {
+                    self.unification_solved.insert(name, ty);
+                }
+                Constraint::UnifyError(error) => {
+                    self.unification_errors.push(error);
+                }
             }
         }
 
@@ -73,8 +77,11 @@ impl<'context> Solve<'context> {
     }
 
     pub fn solve(&mut self) {
-        while let Ok(constraint) = self.context.constraints.pop() {
-            self.step(constraint)
+        loop {
+            self.step();
+            if self.context.constraints.is_empty() {
+                break;
+            }
         }
     }
 }
